9055a7a1b0798c207d8290adfcbe3cf128682d79
Alexander Kluge <developer@akluge.de>
2014-08-31 19:05:48 +0200

    Added aclextras plugin

--- app/Config/bootstrap.php
+++ app/Config/bootstrap.php
@@ -71,6 +71,7 @@ Cache::config('default', array('engine' => 'File'));
  */
 CakePlugin::load('DebugKit');
 CakePlugin::load('Localized');
+CakePlugin::load('AclExtras');
 
 /**
  * You can attach event listeners to the request lifecycle as Dispatcher Filter. By default CakePHP bundles two filters:
--- app/Controller/AppController.php
+++ app/Controller/AppController.php
@@ -32,6 +32,22 @@ App::uses('Controller', 'Controller');
  */
 class AppController extends Controller {
 	
-	public $components = array('DebugKit.Toolbar');
+	public $components = array('DebugKit.Toolbar',
+		'Session',
+                'Cookie',
+		'Acl',
+		'Auth'
+	);		
+	
+	public function beforeFilter() {
+		$this->Auth->authorize = 'actions';
+		$this->Auth->actionPath = 'controllers/';
+		
+		// allow all index and view pages
+		//$this->Auth->allow('index', 'view');
+		
+		// allow everything
+		$this->Auth->allow();
+	}
 	
 }
